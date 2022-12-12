cb2b2a84b2a3383d183d68cf6daf314c616477e6
candiceevemiller <candice.eve.miller@gmail.com>
2022-12-11 22:53:39 -0500

    removed noloop(), added a timer to draw circle, moved initial conditions out of randomWalk() so it doesn't overwrite them. Changed background color.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
+// Constants
+let STEP_SIZE = 5;
+let NUM_STEPS = 1;
+let RADIUS = 10;
+
+// Starting Values
+let cX = Math.floor(WIDTH / 2);
+let cY = Math.floor(HEIGHT / 2);
+let hue = getRandomIntInclusive(0, 360);
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-// TODO add timeout function so it draws slower
 function randomWalk() {
-    // Constants
-    let STEP_SIZE = 5;
-    let NUM_STEPS = 10000;
-    let RADIUS = 10;
-    
-    // Starting Values
-    let cX = Math.floor(WIDTH / 2);
-    let cY = Math.floor(HEIGHT / 2);
-    let hue = 0;
     colorMode(HSB);
-
+    
     for (let i=0; i < NUM_STEPS; i++) {  
     // draw circle
     noStroke(0);  
     fill(hue, 100, 100);
-    circle(cX, cY, RADIUS);
+    setInterval(circle(cX, cY, RADIUS), 1000);
     
     // Get random directions
     let direction = getRandomIntInclusive(0,3);  // this always returns 0,1,2,or 3
@@ -34,34 +34,37 @@ function randomWalk() {
     switch (direction) {
         case 0:
             cX -= STEP_SIZE; // move left
+            cX %= WIDTH;
             break;
         case 1:
             cX += STEP_SIZE; // move right
+            cX %= WIDTH;
             break;
         case 2:
             cY -= STEP_SIZE; // move up
+            cY %= HEIGHT;
             break;
         case 3:
             cY += STEP_SIZE; // move down
+            cY %= HEIGHT;
             break;
     }
     
     if (hueDirection === 1) {
         hue += STEP_SIZE;
+        hue %= 360;
+        break;
     } else {
         hue -= STEP_SIZE;
+        hue %= 360;
+        break;
     }
-    
-    // keep everything within bounds
-    hue %= 360;  // HSV is a polar color space
-    cX %= WIDTH;
-    cY %= HEIGHT;
     }  
 }
 
 function setup() {
 createCanvas(WIDTH, HEIGHT);
-// noLoop(); // p5 will loop code in draw() infinitely without this (commented out because I want it to now that it works)
+background('#222222');
 }
 
 function draw() {
